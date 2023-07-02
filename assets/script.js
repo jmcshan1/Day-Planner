b8cee32809b2f0315c9fb77c8b8b429949ea8518
jmcshan1 <jmcshan01@gmail.com>
2023-07-01 23:07:12 -0500

    saves user input in text area to local storage in corresponding key

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,18 @@ $(function () {
     timeBlock4EL = $("#hour-16"),
     timeBlock5EL = $("#hour-17"),
   ]
+
+  var dayPlans = {
+    hour9: '',
+    hour10: '',
+    hour11: '',
+    hour12: '',
+    hour13: '',
+    hour14: '',
+    hour15: '',
+    hour16: '',
+    hour17: '',
+  };
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -23,6 +35,15 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  saveBtnEl.on("click",function(event){
+    parentDiv = $(event.target).parent();
+    hourSaved = "hour" + parentDiv.attr("id").slice(5);
+    userInput = parentDiv.children('textarea').val();
+
+    dayPlans[hourSaved] = userInput;
+    localStorage.setItem("dayPlans", JSON.stringify(dayPlans));
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
